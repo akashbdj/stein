8a8fd19e9694716e1cb510853227d849e6b09440
Akash Bhardwaj <akashbdj@gmail.com>
2017-09-24 00:31:39 +0530

    Priority Queue introduced

---
+++ javascript/data-structures/priority-queue/index.js
@@ -0,0 +1,18 @@
+/**
+ *  Priority Queue:
+ *      Priority Queue is an extension of queue with following properties.
+ *          1) Every item has a priority associated with it.
+ *          2) An element with high priority is dequeued before an element with low priority.
+ *          3) If two elements have the same priority, they are served according to their order in the queue.
+ *
+ *      A typical priority queue supports following operations.
+ *          ➤ insert(item, priority): Inserts an item with given priority.
+ *          ➤ getHighestPriority(): Returns the highest priority item.
+ *          ➤ deleteHighestPriority(): Removes the highest priority item.
+ *
+ *      Applications of Priority Queue:
+ *          1) CPU Scheduling
+ *          2) Graph algorithms like Dijkstra’s shortest path algorithm, Prim’s Minimum Spanning Tree, etc
+ *          3) All queue applications where priority is involved.
+ *
+ */
